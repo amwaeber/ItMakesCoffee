1d191806b60d4feda0c3adec53c9a02293b7fcfc
awaeberlc <60709472+awaeberlc@users.noreply.github.com>
2020-04-11 18:39:36 +0100

    added selection window

--- user_interfaces/folder_layout.py
+++ user_interfaces/folder_layout.py
@@ -1,5 +1,5 @@
 import os
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.Qt import Qt
 
 from utility.config import paths
@@ -11,9 +11,11 @@ class FolderLayout(QtWidgets.QHBoxLayout):
     def __init__(self, parent=None):
         super(FolderLayout, self).__init__(parent)
 
-        self.selected_file_path = ''
+        self.picked_file_path = ''
         self.reference_files = list()
+        self.selection_files = list()
 
+        self.folder_group_box = QtWidgets.QGroupBox('Folders')
         self.folder_model = QtWidgets.QFileSystemModel()
         self.folder_model.setRootPath('')
         self.folder_tree = QtWidgets.QTreeView()
@@ -25,6 +27,9 @@ class FolderLayout(QtWidgets.QHBoxLayout):
         self.folder_tree.setSortingEnabled(True)
         self.folder_tree.setColumnWidth(0, 500)
         self.folder_tree.clicked.connect(self.select_files)
+        self.folder_layout = QtWidgets.QHBoxLayout()
+        self.folder_layout.addWidget(self.folder_tree)
+        self.folder_group_box.setLayout(self.folder_layout)
 
         self.add_reference_button = QtWidgets.QPushButton(
             QtGui.QIcon(os.path.join(paths['icons'], 'right_double_arrow.png')), '')
@@ -33,56 +38,95 @@ class FolderLayout(QtWidgets.QHBoxLayout):
         self.remove_reference_button = QtWidgets.QPushButton(
             QtGui.QIcon(os.path.join(paths['icons'], 'left_double_arrow.png')), '')
         self.remove_reference_button.clicked.connect(self.remove_reference)
-        self.add_reference_button.setToolTip('Remove reference')
+        self.remove_reference_button.setToolTip('Remove reference')
 
         self.reference_group_box = QtWidgets.QGroupBox('Reference curve')
         self.reference_tree = QtWidgets.QTreeWidget()
+        self.reference_tree.header().hide()
         self.reference_layout = QtWidgets.QHBoxLayout()
         self.reference_layout.addWidget(self.reference_tree)
         self.reference_group_box.setLayout(self.reference_layout)
 
-        self.reference_tree.header().hide()
+        self.add_selection_button = QtWidgets.QPushButton(
+            QtGui.QIcon(os.path.join(paths['icons'], 'right_double_arrow.png')), '')
+        self.add_selection_button.clicked.connect(self.add_selection)
+        self.add_selection_button.setToolTip('Add selection')
+        self.remove_selection_button = QtWidgets.QPushButton(
+            QtGui.QIcon(os.path.join(paths['icons'], 'left_double_arrow.png')), '')
+        self.remove_selection_button.clicked.connect(self.remove_selection)
+        self.remove_selection_button.setToolTip('Remove selection')
+
+        self.selection_group_box = QtWidgets.QGroupBox('Curves for Analysis')
+        self.selection_tree = QtWidgets.QTreeWidget()
+        self.selection_layout = QtWidgets.QHBoxLayout()
+        self.selection_layout.addWidget(self.selection_tree)
+        self.selection_group_box.setLayout(self.selection_layout)
+        self.selection_tree.header().hide()
 
         vbox = QtWidgets.QVBoxLayout()
+        vbox.addStretch(1)
         vbox.addWidget(self.add_reference_button)
         vbox.addWidget(self.remove_reference_button)
+        vbox.addStretch(1)
+        hbox = QtWidgets.QHBoxLayout()
+        hbox.addLayout(vbox)
+        hbox.addWidget(self.reference_group_box)
 
-        self.addWidget(self.folder_tree)
-        self.addLayout(vbox)
-        self.addWidget(self.reference_group_box)
-        # self.addWidget(self.selection_tree)
+        vbox2 = QtWidgets.QVBoxLayout()
+        vbox2.addStretch(1)
+        vbox2.addWidget(self.add_selection_button)
+        vbox2.addWidget(self.remove_selection_button)
+        vbox2.addStretch(1)
+        hbox2 = QtWidgets.QHBoxLayout()
+        hbox2.addLayout(vbox2)
+        hbox2.addWidget(self.selection_group_box)
+
+        vbox_large = QtWidgets.QVBoxLayout()
+        vbox_large.addLayout(hbox)
+        vbox_large.addLayout(hbox2)
+
+        self.addWidget(self.folder_group_box)
+        self.addLayout(vbox_large)
 
     def add_reference(self):
-        csv_list = get_list_of_csv(self.selected_file_path)
+        csv_list = get_list_of_csv(self.picked_file_path)
         if csv_list:
             self.reference_files = self.reference_files + csv_list
             self.reference_files = list(dict.fromkeys(self.reference_files))
             self.update_tree(self.reference_tree, self.reference_files)
-        print(self.reference_files)
+
+    def add_selection(self):
+        csv_list = get_list_of_csv(self.picked_file_path)
+        if csv_list:
+            self.selection_files = self.selection_files + csv_list
+            self.selection_files = list(dict.fromkeys(self.selection_files))
+            self.update_tree(self.selection_tree, self.selection_files)
 
     def remove_reference(self):
         pass
 
+    def remove_selection(self):
+        pass
+
     def select_files(self, signal):
-        self.selected_file_path = self.folder_tree.model().filePath(signal)
+        self.picked_file_path = self.folder_tree.model().filePath(signal)
 
     def update_tree(self, tree_widget, file_paths):
         tree_widget.clear()
-        i_dir = 0
+        # i_dir = 0
         for path in file_paths:
-            if os.path.isdir(path):
-                folder = QtWidgets.QTreeWidgetItem(tree_widget)
-                folder.setText(0, path)
-                folder.setFlags(folder.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
-                i_dir = i_dir + 1
-            elif os.path.isfile(path):
-                if i_dir == 0:
-                    file = QtWidgets.QTreeWidgetItem(tree_widget)
-                else:
-                    file = QtWidgets.QTreeWidgetItem(folder)
+            dirname = os.path.dirname(path)
+            folder = tree_widget.findItems(dirname, QtCore.Qt.MatchExactly)
+            if folder:
+                file = QtWidgets.QTreeWidgetItem(folder[0])
                 file.setText(0, os.path.basename(path))
                 file.setFlags(file.flags() | Qt.ItemIsUserCheckable)
                 file.setCheckState(0, Qt.Unchecked)
-
-'C:/Users/amwae/Python/Lambda/test/03-04-2020/PV 1st/IV Characterizer-1 Run 667 2020-04-03T15.28.37.csv'
-'C:/Users/amwae/Python/Lambda/test/03-04-2020/PV 1st\\IV Characterizer-1 Run 667 2020-04-03T15.28.37.csv'
+            else:
+                folder = QtWidgets.QTreeWidgetItem(tree_widget)
+                folder.setText(0, dirname)
+                folder.setFlags(folder.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
+                file = QtWidgets.QTreeWidgetItem(folder)
+                file.setText(0, os.path.basename(path))
+                file.setFlags(file.flags() | Qt.ItemIsUserCheckable)
+                file.setCheckState(0, Qt.Unchecked)
