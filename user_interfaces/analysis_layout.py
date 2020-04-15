67fd9ccc284f607e99b7d43362d30e91ab781213
awaeberlc <60709472+awaeberlc@users.noreply.github.com>
2020-04-15 16:11:13 +0100

    added UI elements for table

--- user_interfaces/analysis_layout.py
+++ user_interfaces/analysis_layout.py
@@ -2,11 +2,26 @@ import os
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.Qt import Qt
 
+from utility.config import paths
+
 
 class AnalysisLayout(QtWidgets.QHBoxLayout):
 
     def __init__(self, parent=None):
         super(AnalysisLayout, self).__init__(parent)
+
+        self.refresh_button = QtWidgets.QPushButton(
+            QtGui.QIcon(os.path.join(paths['icons'], 'refresh.png')), '')
+        self.refresh_button.clicked.connect(self.load_selection)
+        self.refresh_button.setToolTip('Load selected files')
+        self.toggle_stats_button = QtWidgets.QPushButton(
+            QtGui.QIcon(os.path.join(paths['icons'], 'statistics.png')), '')
+        self.toggle_stats_button.clicked.connect(self.toggle_table)
+        self.toggle_stats_button.setToolTip('Show average / relative values')
+        self.save_tables_button = QtWidgets.QPushButton(
+            QtGui.QIcon(os.path.join(paths['icons'], 'save.png')), '')
+        self.save_tables_button.clicked.connect(self.save_tables)
+        self.save_tables_button.setToolTip('Save as csv')
 
         self.statistics_group_box = QtWidgets.QGroupBox('File statistics')
 
@@ -23,8 +38,27 @@ class AnalysisLayout(QtWidgets.QHBoxLayout):
         self.statistics_table.setItem(3, 1, QtWidgets.QTableWidgetItem("Cell (4,2)"))
 
         vbox1 = QtWidgets.QVBoxLayout()
-        vbox1.addWidget(self.statistics_table)
-        self.statistics_group_box.setLayout(vbox1)
-        self.addWidget(self.statistics_group_box)
+        vbox1.addWidget(self.refresh_button)
+        vbox1.addWidget(self.toggle_stats_button)
+        vbox1.addWidget(self.save_tables_button)
+        vbox1.addStretch(1)
+        vbox2 = QtWidgets.QVBoxLayout()
+        vbox2.addWidget(self.statistics_table)
+        self.statistics_group_box.setLayout(vbox2)
+        hbox = QtWidgets.QHBoxLayout()
+        hbox.addLayout(vbox1)
+        hbox.addWidget(self.statistics_group_box)
+        self.addLayout(hbox)
 
+    def load_selection(self):
+        # Load file data for selection from folder tab
         # self.files = self.parent().tab3.
+        pass
+
+    def save_tables(self):
+        # save both average and relative tables
+        pass
+
+    def toggle_table(self):
+        # switch between showing average data by experiment and relative change vs reference
+        pass
--- utility/create_config.py
+++ utility/create_config.py
@@ -9,7 +9,7 @@ def write_config():
     config = ConfigParser()
 
     config['globals'] = {'progname': 'ItMakesCoffee',
-                         'progversion': '2020.04.13.01'
+                         'progversion': '2020.04.15.01'
                          }
 
     config['paths'] = {'icons': os.path.join(project_path, 'icons'),
