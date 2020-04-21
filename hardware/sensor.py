fa044b0e916b892692d32cd35b9d818126a668d1
awaeberlc <60709472+awaeberlc@users.noreply.github.com>
2020-04-21 14:44:28 +0100

    show figures in claibration tab

--- hardware/sensor.py
+++ hardware/sensor.py
@@ -1,3 +1,6 @@
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import numpy as np
 from PyQt5 import QtCore
 import serial
@@ -6,10 +9,11 @@ import time
 
 port = 'COM3'
 timeout = 30
-n_ai = 1  # number of analogue inputs (eventually 5?)
+n_ai = 5  # number of analogue inputs (eventually 5?)
 
 
 class ArduinoSensor(QtCore.QObject):
+    update = QtCore.pyqtSignal()
 
     def __init__(self, dt=10e-1, t=30.0):
         super(QtCore.QObject, self).__init__()
@@ -52,11 +56,60 @@ class ArduinoSensor(QtCore.QObject):
             string_n = b.decode()  # decode byte string into Unicode
             string = string_n.rstrip()  # remove \n and \r
             self.ci[counter] = counter
-            self.ai[0][counter] = float(string)
+            self.ai[counter][0] = float(string)*100  # Celsius
             counter = (counter + 1) % self.ci.size  # reset counter to zero
             time.sleep(0.1)
         self.ser.close()
 
+    def plot(self, target_fig=None, fname=None, chs=None):
+        """
+        plot - plot the result of an ODMR measurement
+        if ax is supplied, plot into ax. Create a new figure otherwise
+        if ax is none (new figure), the new figure is displayed by default.
+        If a filename string fname is given, the figure is saved to this file and display is suppressed
+        fname is an absolute path. No check or indexing is performed to prevent overwriting of existing files
+        """
+        if chs is None:
+            chs = []
+        if target_fig is None:
+            fig = Figure()
+            canvas = FigureCanvas(fig)
+        else:
+            fig = target_fig
+        fig.clear()
+        axes = fig.add_subplot(111)
+        if 'temp' in chs:
+            axes.plot(self.ci*self.dt, self.ai[:, 0]*100, lw=1.3)
+            spread = (self.ai[:, 0].max() - self.ai[:, 0].min())
+            # make sure plotting range is sufficient to display a minimum amount of contrast
+            if spread < 1:
+                spread = 1
+            upper = self.ai[:, 0].max() + .1 * spread
+            lower = self.ai[:, 0].min() - .1 * spread
+            axes.set_ylim([lower, upper])
+            axes.set_xlabel("Time (s)")
+            axes.set_ylabel("Temperature (C)")
+        if 'power' in chs:
+            axes.plot(self.ci*self.dt, self.ai[:, 1], lw=1.3)
+            axes.plot(self.ci*self.dt, self.ai[:, 2], lw=1.3)
+            axes.plot(self.ci*self.dt, self.ai[:, 3], lw=1.3)
+            axes.plot(self.ci*self.dt, self.ai[:, 4], lw=1.3)
+            spread = (self.ai[:, 1].max()-self.ai[:, 1].min())
+            if spread < .01:
+                spread = .01
+            upper = self.ci.max() + .1*spread
+            lower = self.ci.min() - .1*spread
+            axes.set_ylim([lower, upper])
+            axes.set_xlabel("Time (s)")
+            axes.set_ylabel("Diode Voltage (V)")
+        if target_fig is None:
+            if fname is not None:
+                fig.tight_layout()
+                fig.savefig(fname)
+                plt.close(fig)
+            else:
+                fig.show()
+        return fig
 
 
 
--- user_interfaces/calibration_tab.py
+++ user_interfaces/calibration_tab.py
@@ -1,9 +1,52 @@
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+import os
 from PyQt5 import QtWidgets, QtGui, QtCore
+
+import hardware.sensor as sensor
+from utility.config import paths
 
 
 class Calibration(QtWidgets.QWidget):
+    update_plt = QtCore.pyqtSignal()
 
     def __init__(self, parent=None):
         super(Calibration, self).__init__(parent)
+
+        self.temperature_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        self.temperature_canvas.figure.tight_layout(pad=0.3)
+        self.update_plt.connect(self.temperature_canvas.figure.canvas.draw)
+        self.power_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        self.power_canvas.figure.tight_layout(pad=0.3)
+        self.update_plt.connect(self.power_canvas.figure.canvas.draw)
+
+        self.start_button = QtWidgets.QPushButton(
+            QtGui.QIcon(os.path.join(paths['icons'], 'start.png')), '')
+        self.start_button.clicked.connect(self.start)
+        self.start_button.setToolTip('Run acquisition')
+        self.stop_button = QtWidgets.QPushButton(
+            QtGui.QIcon(os.path.join(paths['icons'], 'start.png')), '')
+        self.stop_button.clicked.connect(self.stop)
+        self.stop_button.setToolTip('Stop acquisition')
+
+        hbox = QtWidgets.QHBoxLayout()
+        hbox.addWidget(self.temperature_canvas)
+        hbox.addWidget(self.power_canvas)
+        self.setLayout(hbox)
+
+        self.mes = sensor.ArduinoSensor(dt=10e-1, t=30.0)
+        self.register(self.mes)
+        self.mes.update.emit()
+
+    def register(self, mes):
+        self.mes = mes
+        self.mes.update.connect(self.update)
+
+    def update(self):
+        if not self.mes:
+            return
+        self.mes.plot(self.temperature_canvas.figure, chs=['temp'])
+        self.mes.plot(self.power_canvas.figure, chs=['power'])
+        self.update_plt.emit()
 
