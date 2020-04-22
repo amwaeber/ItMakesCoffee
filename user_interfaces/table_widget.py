830dbee6293538c481ea27112d3205ee5a9cdb5e
awaeberlc <60709472+awaeberlc@users.noreply.github.com>
2020-04-22 15:46:45 +0100

    merged and renamed files

--- user_interfaces/table_widget.py
+++ user_interfaces/table_widget.py
@@ -1,10 +1,10 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot
 
-from user_interfaces.analysis_layout import Analysis
+from user_interfaces.analysis_tab import Analysis
 from user_interfaces.calibration_tab import Calibration
 from user_interfaces.experiment_tab import Experiment
-from user_interfaces.folder_layout import Folders
+from user_interfaces.folder_tab import Folders
 
 
 class TableWidget(QtWidgets.QWidget):
