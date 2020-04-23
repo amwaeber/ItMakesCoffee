da8c468226132d52e5a77f276e902f604f9955f3
awaeberlc <60709472+awaeberlc@users.noreply.github.com>
2020-04-23 10:41:24 +0100

    restore full-screen at startup

--- user_interfaces/main_window.py
+++ user_interfaces/main_window.py
@@ -14,10 +14,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def init_ui(self):
 
-        self.resize(800, 600)
-        self.showMaximized()
         self.setWindowIcon(QtGui.QIcon(os.path.join(paths['icons'], 'coffee.png')))
         self.setWindowTitle("%s %s" % (global_confs['progname'], global_confs['progversion']))
 
         self.table_widget = TableWidget(self)  # create multiple document interface widget
         self.setCentralWidget(self.table_widget)
+        self.showMaximized()
