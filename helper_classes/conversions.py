877100f40b55856f08714b0d951a9fb8816b34b9
awaeberlc <60709472+awaeberlc@users.noreply.github.com>
2020-05-29 14:09:18 +0100

    Adjusted temperature calibration for use of thermistor

--- helper_classes/conversions.py
+++ helper_classes/conversions.py
@@ -1,11 +1,21 @@
+import numpy as np
+
+
 def digital_to_voltage(value=0, bits=10, voltage_range=5):
     return value / (2**bits-1) * voltage_range
 
 
-def voltage_to_temperature(voltage=0):
+def voltage_to_temperature_thermocouple(voltage=0):
     intrinsic_conversion = 100  # 1V = 100 C
     gain = 6.82
     return voltage * intrinsic_conversion / gain
+
+
+def voltage_to_temperature(voltage=0, voltage_range=5):  # with 100kOhm thermistor
+    serial_resistance = 58
+    resistance = voltage * serial_resistance / (voltage_range - voltage)
+    temperature = - 21.39443 * np.log(resistance) + 123.62807
+    return temperature
 
 
 def voltage_to_power(voltage=0):
