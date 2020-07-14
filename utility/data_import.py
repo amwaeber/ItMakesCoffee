987033182a3be7650be74dc2fc37ef7d291d6933
awaeberlc <60709472+awaeberlc@users.noreply.github.com>
2020-07-14 14:47:38 +0100

    added inheritance to experiment data objects

--- utility/data_import.py
+++ utility/data_import.py
@@ -16,26 +16,29 @@ average_keys = ['Open Circuit Voltage V_oc (V)', 'Short Circuit Current I_sc (A)
                 'Average Irradiance I_4_avg (W/m2)']
 
 
-class Experiment:
-    def __init__(self, folder_path='.'):
+class DataBundle:
+    def __init__(self, *args, **kwargs):
         self.is_reference = False
         self.is_plotted = False
 
-        self.folder_path = os.path.normpath(folder_path)
-        self.name = os.path.basename(self.folder_path)
-        self.file_path = os.path.join(self.folder_path, 'experiment.pkl')
-        n_csv = folders.get_number_of_csv(self.folder_path)
+        self.file_path = os.path.normpath(kwargs.get('file_path', '.'))
+        self.folder_path = os.path.normpath(kwargs.get('folder_path', '.'))
+        self.name = 'default'
 
-        if os.path.exists(self.file_path):
-            try:
-                self.version, self.time, self.film_thickness, self.film_area, self.n_traces, self.traces, \
-                    self.values, self.average_data, self.reference_path, self.efficiencies = self.load_pickle()
-                if any([self.version != __version__, n_csv != self.n_traces]):
-                    raise ValueError
-            except ValueError:  # mismatch in version or traces, or more parameters added since previous version
-                self.import_from_files()
-        else:
-            self.import_from_files()
+        self.version = __version__
+        self.time = 0
+        self.film_thickness = -1
+        self.film_area = -1
+
+        self.n_traces = 0
+        self.traces = {}
+        self.average_data = pd.DataFrame(columns=['Index', 'Time (s)', 'Voltage (V)', 'Current (A)', 'Power (W)',
+                                                  'Temperature (C)', 'Irradiance 1 (W/m2)', 'Irradiance 2 (W/m2)',
+                                                  'Irradiance 3 (W/m2)', 'Irradiance 4 (W/m2)'])
+        self.values = {key: [0, 0] for key in average_keys}
+
+        self.reference_path = ''
+        self.efficiencies = {key: [0, 0] for key in efficiency_keys}
 
         self.plot_categories = {'Experiment': str(self.name),
                                 'Film Thickness': str(self.film_thickness),
@@ -43,39 +46,11 @@ class Experiment:
                                 'Time': str(self.time)
                                 }
 
+    def import_from_pickle(self, *args, **kwargs):
+        pass
+
     def import_from_files(self, *args, **kwargs):
-        self.load_settings()
-        self.version = __version__
-
-        self.n_traces = folders.get_number_of_csv(self.folder_path)  # 1st par: new format, 2nd par: kickstart format
-        self.traces = {}
-        for trace in range(self.n_traces[0]):
-            key = 'IV_Curve_%s' % str(trace)
-            self.traces[key] = Trace(os.path.join(self.folder_path, key + '.csv'), self, key)
-        if self.n_traces[1] > 0:  # Import Kickstart files if there are any
-            kickstart_files = folders.get_kickstart_paths(self.folder_path)
-            for itrace, trace in enumerate(range(self.n_traces[0], self.n_traces[0] + self.n_traces[1])):
-                key = 'IV_Curve_%s' % str(trace)
-                self.traces[key] = KickstartTrace(os.path.join(self.folder_path, kickstart_files[itrace]),
-                                                  self, key)
-        self.update_average()
-        self.update_reference(None)  # set reference and efficiencies to default
-
-    def load_settings(self):
-        try:
-            with open(os.path.join(self.folder_path, 'Settings.txt')) as f:
-                file_contents = f.readlines()
-                self.time = file_contents[0].strip('\n')
-                if file_contents[2].startswith("Film"):
-                    self.film_thickness = file_contents[3].strip('\n').split(' ')[-1]
-                    self.film_area = file_contents[4].strip('\n').split(' ')[-1]
-                else:
-                    self.film_thickness = -1
-                    self.film_area = -1
-        except FileNotFoundError:
-            self.time = folders.get_datetime(self.folder_path)
-            self.film_thickness = -1
-            self.film_area = -1
+        pass
 
     def update_average(self):
         combined_data = pd.concat((trace.data for trace in self.traces.values() if trace.is_included))
@@ -102,6 +77,13 @@ class Experiment:
             return [100 * (self.values[key][0] - ref_experiment.values[key][0]) / ref_experiment.values[key][0],
                     100 * self.values[key][1] / ref_experiment.values[key][0]]
 
+    def update_plot_categories(self):
+        self.plot_categories = {'Experiment': str(self.name),
+                                'Film Thickness': str(self.film_thickness),
+                                'Film Area': str(self.film_area),
+                                'Time': str(self.time)
+                                }
+
     def save_pickle(self):
         with open(self.file_path, 'wb') as f:
             pickle.dump([self.version, self.time, self.film_thickness, self.film_area, self.n_traces, self.traces,
@@ -112,15 +94,75 @@ class Experiment:
             return pickle.load(f)
 
 
-class Group(Experiment):
-    def __init__(self, file_path='.', trace_paths=None):
-        self.is_reference = False
-        self.is_plotted = False
+class Experiment(DataBundle):
+    def __init__(self, folder_path='.'):
+        super().__init__(folder_path=folder_path)
 
-        self.file_path = os.path.join(file_path)
+        self.name = os.path.basename(self.folder_path)
+        self.file_path = os.path.join(self.folder_path, 'experiment.pkl')
+
+        self.import_from_pickle()
+        self.update_plot_categories()
+
+    def import_from_pickle(self):
+        n_csv = folders.get_number_of_csv(self.folder_path)
+        if os.path.exists(self.file_path):
+            try:
+                self.version, self.time, self.film_thickness, self.film_area, self.n_traces, self.traces, \
+                    self.values, self.average_data, self.reference_path, self.efficiencies = self.load_pickle()
+                if any([self.version != __version__, n_csv != self.n_traces]):
+                    raise ValueError
+            except ValueError:  # mismatch in version or traces, or more parameters added since previous version
+                self.import_from_files()
+        else:
+            self.import_from_files()
+
+    def import_from_files(self):
+        self.load_settings()
+        self.version = __version__
+
+        self.n_traces = folders.get_number_of_csv(self.folder_path)  # 1st par: new format, 2nd par: kickstart format
+        self.traces = {}
+        for trace in range(self.n_traces[0]):
+            key = 'IV_Curve_%s' % str(trace)
+            self.traces[key] = Trace(os.path.join(self.folder_path, key + '.csv'), self.name, key)
+        if self.n_traces[1] > 0:  # Import Kickstart files if there are any
+            kickstart_files = folders.get_kickstart_paths(self.folder_path)
+            for itrace, trace in enumerate(range(self.n_traces[0], self.n_traces[0] + self.n_traces[1])):
+                key = 'IV_Curve_%s' % str(trace)
+                self.traces[key] = KickstartTrace(os.path.join(self.folder_path, kickstart_files[itrace]),
+                                                  self.name, key)
+        self.update_average()
+        self.update_reference(None)  # set reference and efficiencies to default
+
+    def load_settings(self):
+        try:
+            with open(os.path.join(self.folder_path, 'Settings.txt')) as f:
+                file_contents = f.readlines()
+                self.time = file_contents[0].strip('\n')
+                if file_contents[2].startswith("Film"):
+                    self.film_thickness = file_contents[3].strip('\n').split(' ')[-1]
+                    self.film_area = file_contents[4].strip('\n').split(' ')[-1]
+                else:
+                    self.film_thickness = -1
+                    self.film_area = -1
+        except FileNotFoundError:
+            self.time = folders.get_datetime(self.folder_path)
+            self.film_thickness = -1
+            self.film_area = -1
+
+
+class Group(DataBundle):
+    def __init__(self, file_path='.', trace_paths=None):
+        super().__init__(file_path=file_path)
+
         self.folder_path = os.path.dirname(self.file_path)
         self.name = os.path.basename(self.file_path)
 
+        self.import_from_pickle(trace_paths)
+        self.update_plot_categories()
+
+    def import_from_pickle(self, trace_paths):
         if os.path.exists(self.file_path):
             try:
                 self.version, self.time, self.film_thickness, self.film_area, self.n_traces, self.traces, \
@@ -129,12 +171,6 @@ class Group(Experiment):
                 self.failed_import()
         else:
             self.import_from_files(trace_paths)
-
-        self.plot_categories = {'Experiment': str(self.name),
-                                'Film Thickness': str(self.film_thickness),
-                                'Film Area': str(self.film_area),
-                                'Time': str(self.time)
-                                }
 
     def failed_import(self):
         print('Could not import %s.' % self.file_path)
@@ -146,9 +182,9 @@ class Group(Experiment):
         for itrace, trace_path in enumerate(trace_paths):
             key = 'IV_Curve_%s' % str(itrace)
             if os.path.basename(trace_path).startswith('IV_Curve_'):
-                self.traces[key] = Trace(trace_path, self, key)
+                self.traces[key] = Trace(trace_path, self.name, key)
             else:
-                self.traces[key] = KickstartTrace(trace_path, self, key)
+                self.traces[key] = KickstartTrace(trace_path, self.name, key)
             if itrace == 0:
                 self.time = self.traces[key].time
                 self.film_thickness = self.traces[key].film_thickness
@@ -162,13 +198,76 @@ class Group(Experiment):
         self.update_reference(None)  # set reference and efficiencies to default
 
 
-class Trace:
-    def __init__(self, data_path='.', experiment=None, key=None):
+class Data:
+    def __init__(self, data_path='.', *args, **kwargs):
         self.data_path = os.path.normpath(data_path)
-        self.name = key
+        self.name = kwargs.get('key', 'default')
+        self.experiment = kwargs.get('experiment', 'none')
         self.is_included = True
-        self.experiment = experiment.name
+
+        self.film_thickness = -1
+        self.film_area = -1
         self.load_settings()
+        self.data = pd.DataFrame(columns=['Index', 'Time (s)', 'Voltage (V)', 'Current (A)', 'Power (W)',
+                                          'Temperature (C)', 'Irradiance 1 (W/m2)', 'Irradiance 2 (W/m2)',
+                                          'Irradiance 3 (W/m2)', 'Irradiance 4 (W/m2)'])
+        self.time = 0
+        self.values = {key: [0, 0] for key in average_keys}
+
+    def load_settings(self):
+        try:
+            with open(os.path.join(os.path.dirname(self.data_path), 'Settings.txt')) as f:
+                file_contents = f.readlines()
+                if file_contents[2].startswith("Film"):
+                    self.film_thickness = file_contents[3].strip('\n').split(' ')[-1]
+                    self.film_area = file_contents[4].strip('\n').split(' ')[-1]
+                else:
+                    self.film_thickness = -1
+                    self.film_area = -1
+        except FileNotFoundError:
+            self.film_thickness = -1
+            self.film_area = -1
+
+    def get_voc(self):
+        try:
+            voc = self.data.loc[self.data['Current (A)'].abs() == self.data['Current (A)'].abs().min(),
+                                ['Voltage (V)']].values[0, 0]
+        except IndexError:
+            voc = 0
+        return voc
+
+    def get_isc(self):
+        try:
+            isc = self.data.loc[self.data['Voltage (V)'].abs() == self.data['Voltage (V)'].abs().min(),
+                                ['Current (A)']].values[0, 0]
+        except IndexError:
+            isc = 0
+        return isc
+
+    def get_pmax(self):
+        try:
+            if any(self.data['Current (A)'] > 0):
+                pmax = self.data.loc[self.data['Current (A)'] > 0]['Power (W)'].max()
+            else:
+                pmax = 0
+        except IndexError:
+            pmax = 0
+        return pmax
+
+    def get_fill_factor(self):
+        vocisc = self.get_voc() * self.get_isc()
+        if vocisc <= 0:
+            return 0
+        else:
+            return abs(self.get_pmax() / vocisc)
+
+
+class Trace(Data):
+    def __init__(self, data_path='.', experiment=None, key=None):
+        super().__init__(data_path=data_path, experiment=experiment, key=key)
+        self.csv_import()
+
+    def csv_import(self):
         self.data = pd.read_csv(self.data_path, header=None, index_col=0, skiprows=3,
                                 names=["Index", "Time (s)", "Voltage (V)", "Current (A)", "Current Std (A)",
                                        "Resistance (Ohm)", "Power (W)", "Temperature (C)", "Irradiance 1 (W/m2)",
@@ -182,93 +281,23 @@ class Trace:
         for key, col in zip(average_keys[4:], self.data.columns.values[4:]):
             self.values[key] = [self.data[col].mean(), self.data[col].std()]
 
-    def get_voc(self):
-        return self.data.loc[self.data['Current (A)'].abs() == self.data['Current (A)'].abs().min(),
-                             ['Voltage (V)']].values[0, 0]
 
-    def get_isc(self):
-        return self.data.loc[self.data['Voltage (V)'].abs() == self.data['Voltage (V)'].abs().min(),
-                             ['Current (A)']].values[0, 0]
-
-    def get_pmax(self):
-        pmax = self.data.loc[self.data['Current (A)'] > 0]['Power (W)'].max()
-        return pmax if not np.isnan(pmax) else 0.0
-
-    def get_fill_factor(self):
-        vocisc = self.get_voc() * self.get_isc()
-        if vocisc == 0:
-            return 0.0
-        else:
-            return abs(self.get_pmax() / vocisc)
-
-    def load_settings(self):
-        try:
-            with open(os.path.join(os.path.dirname(self.data_path), 'Settings.txt')) as f:
-                file_contents = f.readlines()
-                if file_contents[2].startswith("Film"):
-                    self.film_thickness = file_contents[3].strip('\n').split(' ')[-1]
-                    self.film_area = file_contents[4].strip('\n').split(' ')[-1]
-                else:
-                    self.film_thickness = -1
-                    self.film_area = -1
-        except FileNotFoundError:
-            self.film_thickness = -1
-            self.film_area = -1
-
-
-class KickstartTrace:
+class KickstartTrace(Data):
     def __init__(self, data_path='.', experiment=None, key=None):
-        self.data_path = os.path.normpath(data_path)
-        self.name = key
-        self.is_included = True
-        self.experiment = experiment.name
-        self.load_settings()
+        super().__init__(data_path=data_path, experiment=experiment, key=key)
+        self.csv_import()
+
+    def csv_import(self):
         self.data = pd.read_csv(self.data_path, sep=',', header=0, index_col=0, skiprows=33,
                                 names=["Time (s)", "Voltage (V)", "Current (A)"])
         self.time = time.mktime(time.strptime(os.path.basename(self.data_path).split(' ')[-1], "%Y-%m-%dT%H.%M.%S.csv"))
-        self.data['Index'] = -1
         self.data['Time (s)'] = self.data['Time (s)'] + self.time
         self.data['Current (A)'] = - self.data['Current (A)']
         self.data['Power (W)'] = self.data['Voltage (V)'] * self.data['Current (A)']
-        for item in ["Temperature (C)", "Irradiance 1 (W/m2)", "Irradiance 2 (W/m2)", "Irradiance 3 (W/m2)",
+        for item in ['Index', "Temperature (C)", "Irradiance 1 (W/m2)", "Irradiance 2 (W/m2)", "Irradiance 3 (W/m2)",
                      "Irradiance 4 (W/m2)"]:
             self.data[item] = -1
-        self.values = {'Open Circuit Voltage V_oc (V)': [self.get_voc(), 0],
-                       'Short Circuit Current I_sc (A)': [self.get_isc(), 0],
-                       'Maximum Power P_max (W)': [self.get_pmax(), 0],
-                       'Fill Factor': [self.get_fill_factor(), 0]}
-        for key in average_keys[4:]:
-            self.values[key] = [-1, 0.0]
-
-    def get_voc(self):
-        return self.data.loc[self.data['Current (A)'].abs() == self.data['Current (A)'].abs().min(),
-                             ['Voltage (V)']].values[0, 0]
-
-    def get_isc(self):
-        return self.data.loc[self.data['Voltage (V)'].abs() == self.data['Voltage (V)'].abs().min(),
-                             ['Current (A)']].values[0, 0]
-
-    def get_pmax(self):
-        pmax = self.data.loc[self.data['Current (A)'] > 0]['Power (W)'].max()
-        return pmax if not np.isnan(pmax) else 0.0
-
-    def get_fill_factor(self):
-        vocisc = self.get_voc() * self.get_isc()
-        if vocisc == 0:
-            return 0.0
-        else:
-            return abs(self.get_pmax() / vocisc)
-
-    def load_settings(self):
-        try:
-            with open(os.path.join(os.path.dirname(self.data_path), 'Settings.txt')) as f:
-                file_contents = f.readlines()
-                if file_contents[2].startswith("Film"):
-                    self.film_thickness = file_contents[3].strip('\n').split(' ')[-1]
-                    self.film_area = file_contents[4].strip('\n').split(' ')[-1]
-                else:
-                    self.film_thickness = -1
-                    self.film_area = -1
-        except FileNotFoundError:
-            self.film_thickness = -1
-            self.film_area = -1
+        self.values['Open Circuit Voltage V_oc (V)'] = [self.get_voc(), 0]
+        self.values['Short Circuit Current I_sc (A)'] = [self.get_isc(), 0]
+        self.values['Maximum Power P_max (W)'] = [self.get_pmax(), 0]
+        self.values['Fill Factor'] = [self.get_fill_factor(), 0]
